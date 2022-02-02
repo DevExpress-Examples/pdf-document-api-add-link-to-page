45b43be80064751375d7babfdc1595a39ab899d2
DevExpressExampleBot <fakeEmail>
2022-02-02 12:22:47 +0300

    Source auto update [skip ci]

--- VB/AddLinkToPage/Program.vb
+++ VB/AddLinkToPage/Program.vb
@@ -1,22 +1,19 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 Imports System.Drawing
 
-
 Namespace AddLinkToPage
-    Friend Class Program
-        Shared Sub Main(ByVal args() As String)
-            Using processor As New PdfDocumentProcessor()
 
+    Friend Class Program
+
+        Shared Sub Main(ByVal args As String())
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Create an empty document.
                 processor.CreateEmptyDocument("..\..\Result.pdf")
-
                 ' Create and draw graphics.
                 Using graphics As PdfGraphics = processor.CreateGraphics()
                     DrawGraphics(graphics)
-
                     ' Create a link to a page specifying link area, the page number and X, Y destinations.
                     graphics.AddLinkToPage(New RectangleF(180, 160, 480, 30), 1, 168, 230)
-
                     ' Render a page with graphics.
                     processor.RenderNewPage(PdfPaperSize.Letter, graphics)
                 End Using
@@ -24,10 +21,9 @@ Namespace AddLinkToPage
         End Sub
 
         Private Shared Sub DrawGraphics(ByVal graphics As PdfGraphics)
-
             ' Draw a text line on the page. 
             Dim black As SolidBrush = CType(Brushes.Black, SolidBrush)
-            Using font As New Font("Times New Roman", 32, FontStyle.Bold)
+            Using font As Font = New Font("Times New Roman", 32, FontStyle.Bold)
                 graphics.DrawString("PDF Document Processor", font, black, 180, 150)
             End Using
         End Sub
